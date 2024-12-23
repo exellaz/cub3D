66d38f2a06f11458b189386acbeba95a116997a6
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-12-23 09:38:34 +0800

    setup minimal mlx functionalities

--- src/main.c
+++ src/main.c
@@ -17,22 +17,24 @@ typedef struct	s_vars
 }				t_vars;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
-int		my_close(int keycode, t_vars *vars);
+int		quit(int keycode, t_vars *vars);
+int		key_hook(int keycode, t_vars *vars);
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	t_vars vars;
 	t_data	img;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 800, 600, "Hello world!");
+	mlx_hook(vars.win, 17, 0, quit, &vars);
+	mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
+	img.img = mlx_new_image(vars.mlx, 800, 600);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_loop(vars.mlx);
 }
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
@@ -43,9 +45,19 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int	my_close(int keycode, t_vars *vars)
+int	quit(int keycode, t_vars *vars)
 {
-	(void)keycode;
+	printf("keycode: %d\n", keycode);
 	mlx_destroy_window(vars->mlx, vars->win);
+	exit(0);
+	return (0);
+}
+
+int key_hook(int keycode, t_vars *vars)
+{
+	(void)vars;
+	printf("keycode: %d\n", keycode);
+	if (keycode == 65307)
+		quit(keycode, vars);
 	return (0);
 }
