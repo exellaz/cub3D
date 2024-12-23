7791f71555478902efe2a86dcfed0f7139749a5a
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-12-23 10:16:43 +0800

    implemented basic events

--- src/main.c
+++ src/main.c
@@ -1,63 +1,15 @@
-#include "mlx.h"
-# include "Libft.h"
-
-typedef struct	s_data
-{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-typedef struct	s_vars
-{
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
-int		quit(int keycode, t_vars *vars);
-int		key_hook(int keycode, t_vars *vars);
+# include "cub3D.h"
 
 int	main(void)
 {
-	t_vars vars;
-	t_data	img;
+	t_mlx m;
+	t_img i;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 800, 600, "Hello world!");
-	mlx_hook(vars.win, 17, 0, quit, &vars);
-	mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
-	img.img = mlx_new_image(vars.mlx, 800, 600);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_loop(vars.mlx);
-}
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
-int	quit(int keycode, t_vars *vars)
-{
-	printf("keycode: %d\n", keycode);
-	mlx_destroy_window(vars->mlx, vars->win);
-	exit(0);
-	return (0);
-}
-
-int key_hook(int keycode, t_vars *vars)
-{
-	(void)vars;
-	printf("keycode: %d\n", keycode);
-	if (keycode == 65307)
-		quit(keycode, vars);
-	return (0);
+	setup_mlx(&m);
+	i.img = mlx_new_image(m.mlx, 800, 600);
+	i.addr = mlx_get_data_addr(i.img, &i.bits_per_pixel, &i.line_length,
+								&i.endian);
+	my_mlx_pixel_put(&i, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(m.mlx, m.win, i.img, 0, 0);
+	mlx_loop(m.mlx);
 }
