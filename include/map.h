0ef7913ddfa5eb2f40fae2942ae6b1f7442af98f
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-12-24 16:00:50 +0800

    implemented prototype for map

--- include/map.h
+++ include/map.h
@@ -6,7 +6,7 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 17:54:34 by we                #+#    #+#             */
-/*   Updated: 2024/12/23 22:35:20 by we               ###   ########.fr       */
+/*   Updated: 2024/12/24 15:59:57 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,17 @@
 
 typedef struct s_list	t_list;
 
+// `texture` is stored in N, S, E, W order
+typedef struct s_map
+{
+	t_list	*map;
+	char	*texture_path[4];
+	int		ceiling_rgb[3];
+	int		floor_rgb[3];
+}	t_map;
+
 t_list	*load_file(int file);
+char	*parse_texture(char *line);
+int		*parse_rgb(char *line);
 
 #endif
