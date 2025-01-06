fc02e6332c56b200a968f6d9ad713401dbe1c766
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2025-01-06 12:55:02 +0800

    refactored draw_square function

--- src/main.c
+++ src/main.c
@@ -8,29 +8,6 @@ char	**get_map(void)
 {
 	char **map;
 
-	// map = malloc(sizeof(char *) * 22);
-	// map[0] = "111111111111111111111";
-	// map[1] = "101000001000000000001";
-	// map[2] = "101010101011111110001";
-	// map[3] = "100010100010000010001";
-	// map[4] = "111110101111101011101";
-	// map[5] = "100000101000101000001";
-	// map[6] = "101111101011101111111";
-	// map[7] = "101000001010100000101";
-	// map[8] = "101011111010111110101";
-	// map[9] = "101010001010100010101";
-	// map[10] = "101110101010101010101";
-	// map[11] = "100000101010001010001";
-	// map[12] = "111111101011111011101";
-	// map[13] = "100000101000100010001";
-	// map[14] = "101110101110101110111";
-	// map[15] = "101000100000100010101";
-	// map[16] = "101011111111111010101";
-	// map[17] = "100010000000000010001";
-	// map[18] = "101010111011111111101";
-	// map[19] = "1N1000001000000000001";
-	// map[20] = "111111111111111111111";
-	// map[21] = NULL;
 	map = malloc(sizeof(char *) * 11);
 	map[0] = "1111111111";
 	map[1] = "1000000001";
@@ -58,29 +35,28 @@ void put_pixel(int x, int y, int color, t_img *img)
 
 void	draw_square(int x, int y, int size, int color, t_img *img)
 {
-	for (int i = 0; i < size; i++)
-		put_pixel(x + i, y, color, img);
-	for (int i = 0; i < size; i++)
-		put_pixel(x, y + i, color, img);
-	for (int i = 0; i < size; i++)
-		put_pixel(x + size, y + i, color, img);
-	for (int i = 0; i < size; i++)
-		put_pixel(x + i, y + size, color, img);
+	int	draw_x;
+	int	draw_y;
 
-	// int	draw_x;
-	// int	draw_y;
-
-	// draw_y = 0;
-	// while (draw_y < size)
-	// {
-	// 	draw_x = 0;
-	// 	while (draw_x < size)
-	// 	{
-	// 		put_pixel(x + draw_x, y + draw_y, color, img);
-	// 		draw_x++;
-	// 	}
-	// 	draw_y++;
-	// }
+	draw_y = 0;
+	while (draw_y < size)
+	{
+		draw_x = 0;
+		if (draw_y == 0 || draw_y == size - 1)
+		{
+			while (draw_x < size)
+			{
+				put_pixel(x + draw_x, y + draw_y, color, img);
+				draw_x++;
+			}
+		}
+		else
+		{
+			put_pixel(x, y + draw_y, color, img);
+			put_pixel(x + size - 1, y + draw_y, color, img);
+		}
+		draw_y++;
+	}
 }
 
 void	draw_map(t_mlx *mlx)
