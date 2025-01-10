c6c6e96138d7e8078846d60f66fa31f4405da6d9
JunYut <tanjunyu8888@gmail.com>
2025-01-10 09:58:05 +0800

    implemented `lst_to_arr()`

--- src/utils.c
+++ src/utils.c
@@ -6,13 +6,30 @@
 /*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 14:23:53 by tjun-yu           #+#    #+#             */
-/*   Updated: 2025/01/03 14:26:03 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2025/01/10 09:57:36 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Libft.h>
 
 #include "utils.h"
+
+char	**lst_to_arr(t_list *lst)
+{
+	char	**arr;
+	int		i;
+
+	arr = (char **)mem_alloc(sizeof(char *) * (ft_lstsize(lst) + 1));
+	i = -1;
+	while (lst)
+	{
+		arr[++i] = lst->content;
+		arr[i][ft_strlen(arr[i]) - 1] = '\0';
+		lst = lst->next;
+	}
+	arr[++i] = NULL;
+	return (arr);
+}
 
 t_list	*skip_empty_lines(t_list *raw)
 {
